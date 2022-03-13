41728def50ce8c39e27ea74864b627d9f9b7d03e
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2022-03-13 10:52:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ class Config(object):
       API_ID = int(os.environ.get("API_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       OWNER_ID = int(os.environ.get("OWNER_ID"))
-
+      MUST_JOIN = os.environ.get('MUST_JOIN', None)
+    if MUST_JOIN.startswith("@"):
+        MUST_JOIN = MUST_JOIN.replace("@", "")
